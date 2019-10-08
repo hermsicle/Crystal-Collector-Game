a79f5f576addbdfc258e53828d90478b6ffb66e4
Herman Liu <hermanliu95@gmail.com>
2019-10-08 14:49:55 -0700

    first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+$(document).ready (function) {
+
+
+
+}
+
+
+//create variables 
+//Create a random number generator for the winning number
+//create random number generator for the four different crystals
+//create a function that would add all the clicked crystals and show the number
+//create a wins and losses variable
+//have it reset every time you win or lose, while keeping the wins and losses on page.
