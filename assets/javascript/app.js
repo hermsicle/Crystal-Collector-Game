c1a903b894f3d38497ab933e2e6730ff0b5028b3
Herman Liu <hermanliu95@gmail.com>
2019-10-09 15:54:20 -0700

    cleaned up javascript and took out some notes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,18 @@
-//create variables 
-//Create a random number generator for the winning number
-//create random number generator for the four different crystals
+
 //create a function that would add all the clicked crystals and show the number
-//create a wins and losses variable
 //have it reset every time you win or lose, while keeping the wins and losses on page.
 
 $(document).ready(function(){
 
-
 //Create a random winning number 
 var winningNum = Math.floor(Math.random() * 100) + 15;
 console.log ('winningNum ' + winningNum);
-
+$('#randomNUmber').html(winningNum);
 
 //create random number generator for each of the gems
-//blueGem :
-
 var blueGem = Math.floor(Math.random() * 12 + 1);
 console.log ("Gem 1: " + blueGem);
+$('#blueGem').html()
 
 var greenGem = Math.floor(Math.random() * 12 + 1);
 console.log ("Gem 2: " + greenGem);
@@ -28,14 +23,15 @@ console.log ("Gem 1: " + redGem);
 var yellowGem = Math.floor(Math.random() * 12 + 1);
 console.log ("Gem 1: " + yellowGem);
 
-
-//Create win and loss and score var
-
+//Create wim, loss, and score
 var win = 0;
+console.log ('wins: '+ win);
 
 var losses = 0;
+console.log ('losses: ' + losses);
 
 var score = 0;
+console.log ('score: ' + score);
 
 
 
